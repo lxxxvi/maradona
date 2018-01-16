2cdf4808685532217f926d4fbc5ebd5fd085a090
Mario Schuettel <github@lxxxvi.ch>
2018-01-16 18:00:13 +0100

    trying to (fake) update prediction

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,32 @@
+const predictedLeftTeamScoreSpan  = function(predictionId) {
+  return document.querySelector("#predicted_left_team_score_span_" + predictionId);
+}
+
+const predictedLeftTeamScoreInput  = function(predictionId) {
+  return document.querySelector("#predicted_left_team_score_input_" + predictionId);
+};
+
+const predictedRightTeamScoreSpan  = function(predictionId) {
+  return document.querySelector("#predicted_right_team_score_span_" + predictionId);
+}
+
+const predictedRightTeamScoreInput  = function(predictionId) {
+  return document.querySelector("#predicted_right_team_score_input_" + predictionId);
+};
+
+
+var updateScore = function(predictionId) {
+  predictedLeftTeamScoreSpan(predictionId).innerHTML  = predictedLeftTeamScoreInput(predictionId).value;
+  predictedRightTeamScoreSpan(predictionId).innerHTML = predictedRightTeamScoreInput(predictionId).value;
+}
+
 document.querySelectorAll("div[data-link]").forEach(function(item, _index, _list) {
   item.onclick = function() { window.location = item.dataset.link; };
+});
+
+document.querySelectorAll(".score-controls").forEach(function(item, _index, _list) {
+  var predictionId = item.id.split("_").reverse()[0];
+  item.oninput = function() {
+    updateScore(predictionId);
+  };
 });
