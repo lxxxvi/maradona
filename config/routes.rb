27e6a80842543d5c3481fcac5b650e627143f354
Mario Schuettel <github@lxxxvi.ch>
2018-01-23 15:22:53 +0100

    remove obsolete mount method call

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # mount ActionCable.server, at: '/cable' REMOVEME?
-
   root to: 'matches#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
