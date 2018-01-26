b72f6acbb51a0097d156984352e3a9e01cb7551e
Mario Schuettel <github@lxxxvi.ch>
2018-01-26 15:09:13 +0100

    Prediction must be before a game's kickoff

--- app/services/match_prediction_updater_service.rb
+++ app/services/match_prediction_updater_service.rb
@@ -1,17 +1,22 @@
 class MatchPredictionUpdaterService
-  attr_reader :prediction
+  attr_reader :prediction, :match
 
   Response = Struct.new(:status, :messages, :payload)
 
   def initialize(match, user)
+    @match = match
     @prediction = user.predictions.of_match(match).first_or_initialize
   end
 
   def update(left_team_score, right_team_score)
-    if @prediction.update(left_team_score: left_team_score, right_team_score: right_team_score)
+    prediction.assign_attributes(
+      left_team_score: left_team_score,
+      right_team_score: right_team_score
+    )
+    if prediction_valid? && prediction.save
       response(:ok, ['Updated successfully'], payload)
     else
-      response(:error, @prediction.errors.to_a, payload)
+      response(:error, prediction.errors.to_a, payload)
     end
   end
 
@@ -27,5 +32,17 @@ class MatchPredictionUpdaterService
       left_team_score: @prediction.left_team_score,
       right_team_score: @prediction.right_team_score
     }
+  end
+
+  def prediction_valid?
+    prediction.valid?
+    if deadline_for_prediction_passed?
+      prediction.errors.add(:base, "Oh no, you're too late to predict that game")
+    end
+    prediction.errors.none?
+  end
+
+  def deadline_for_prediction_passed?
+    match.kickoff_at <= Time.zone.now
   end
 end
