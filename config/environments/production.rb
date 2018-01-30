1d36a2b69203743592becb786f345f572a77c713
Mario Schuettel <github@lxxxvi.ch>
2018-01-30 14:07:39 +0100

    Initial SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'email-smtp.eu-west-1.amazonaws.com',
+    user_name: Rails.application.secrets.smtp_user_name,
+    password: Rails.application.secrets.smtp_password
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: 'referee@tippkick.club' }
+  config.action_mailer.default_url_options = { host: 'tippkick.club' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
