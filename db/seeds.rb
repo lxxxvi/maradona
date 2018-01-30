6a664c87379ad31d682d456e182790c839b340d0
Mario Schuettel <github@lxxxvi.ch>
2018-01-30 11:39:21 +0100

    Initialize Diego

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@ if Rails.env.development?
   Rake::Task['db:fixtures:load'].invoke
 end
 
+# Admin User
+User.find_or_initialize_by(nickname: 'diego.maradona').tap do |diego|
+  diego.email = 'diego.maradona@tippkick.club'
+  diego.save!
+end
+
 this_directory_path  = File.expand_path(File.dirname(__FILE__))
 sql_seeds_directory_path = Pathname.new("#{this_directory_path}/seeds/sql/")
 
