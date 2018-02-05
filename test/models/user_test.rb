d869c9c131cb9c2d6dcb2a42877b9507e02a375e
Mario Schuettel <github@lxxxvi.ch>
2018-02-05 15:32:44 +0100

    Evaluate all predictions for a user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,12 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'collect_points' do
     zinedine = users(:zinedine)
+    prediction = predictions(:prediction_zinedine_por_esp)
+    assert_not prediction.evaluated?
 
-    assert_changes 'zinedine.points_total', to: 8 do
+    assert_changes 'zinedine.points_total', to: 18 do
       zinedine.collect_points!
+      assert prediction.reload.evaluated?
     end
   end
 end
