9860805187bd53f71b3ee959405a25c2e96541f1
mario <github@lxxxvi.ch>
2018-03-06 18:12:14 +0100

    added squads controller test

---
+++ test/controllers/squads_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SquadsControllerTest < ActionDispatch::IntegrationTest
+  test 'diego sees squads index' do
+    assert false
+  end
+
+  test 'anonymuous sees none' do
+    assert false
+  end
+end
