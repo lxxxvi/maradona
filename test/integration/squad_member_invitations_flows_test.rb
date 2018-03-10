8ad62676d0c50726e3dd17a18071b607d56c799f
mario <github@lxxxvi.ch>
2018-03-10 11:12:00 +0100

    WIP, squad invitation flows

---
+++ test/integration/squad_member_invitations_flows_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class SquadMemberInvitationsFlowsTest < ActionDispatch::IntegrationTest
+  test 'diego invites a new member to squad' do
+    sign_in users(:diego)
+    squad = squads(:fifa_100)
+    get squad_path(squad)
+    assert_response :success
+
+    assert_select 'a.btn-primary', 'Invite user to squad'
+
+    get new_squad_member_invitation_path(squad)
+    form_submit_button = css_select('form input[type=submit].btn-primary').first
+    assert_select 'h1', "Invite user to Fifa 100 squad"
+    assert_select 'a.btn-default', 'Cancel'
+    assert_select 'Send invitation', form_submit_button.attr('value').value
+    assert_select 'form input[type=text][name=nickname]'
+
+    post squad_member_invitation_path(squad), params: {
+      user: {
+        nickname: 'pele'
+      }
+    }
+    follow_redirect!
+    assert_response :success
+
+    assert false
+  end
+
+  test 'cannot invite user that has accepted already' do
+    assert false
+  end
+
+  test 'cannot invite user that has rejected' do
+    assert false
+  end
+end
