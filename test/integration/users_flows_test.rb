30bc6e945b2253d5b4b4d7f4809c541b794d5d1f
mario <github@lxxxvi.ch>
2018-03-19 18:24:13 +0100

    display hint if there are unpredicted matches

--- test/integration/users_flows_test.rb
+++ test/integration/users_flows_test.rb
@@ -8,9 +8,22 @@ class UsersFlowsTest < ActionDispatch::IntegrationTest
 
     get root_path
 
-    assert_select '.card .card-body' do
+    assert_select '.ranking .card .card-body' do
       assert_select '.card-subtitle', 'Global ranking'
       assert_select '.card-title', '# 13'
+    end
+  end
+
+  test 'user sees how many predictions are missing' do
+    user = users(:diego)
+
+    sign_in user
+    get root_path
+
+    assert_select '.unpredicted_matches .card' do
+      assert_select '.card-subtitle', 'Hurry!'
+      assert_select '.card-title', 'You have 2 unpredicted matches'
+      assert_select '.card-text a.btn.btn-success', 'Predict them now!'
     end
   end
 end
