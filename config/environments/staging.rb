a1c4ee232daf0f6d3448e1b4ecfb052743f873c8
mario <github@lxxxvi.ch>
2018-03-23 15:38:06 +0100

    SMTP address for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
+    address: Rails.application.secrets.smtp_address,
     domain: 'tippkick.club',
     user_name: Rails.application.secrets.smtp_user_name,
     password: Rails.application.secrets.smtp_password,
