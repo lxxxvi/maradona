5d533f2eea1c8a613975bf9bbe1053ccfc1a91bc
lxxxvi <lxxxvi@users.noreply.github.com>
2018-04-29 15:21:16 +0200

    squads ranking service

---
+++ app/services/update_squads_rankings_service.rb
@@ -0,0 +1,21 @@
+class UpdateSquadsRankingsService
+  def run!
+    ActiveRecord::Base.connection.execute(rank_squads_sql)
+  end
+
+  private
+
+  def rank_squads_sql
+    <<-SQL
+      WITH ranked_squads AS (
+          SELECT id                                                    AS id
+               , rank() OVER (ORDER BY COALESCE(points_average, 0) DESC) AS ranking_position
+            FROM squads
+      )
+      UPDATE squads
+         SET ranking_position = ranked_squads.ranking_position
+        FROM ranked_squads
+       WHERE squads.id = ranked_squads.id
+    SQL
+  end
+end
