50cc26b88c814298aa9e7ea888533fff20f6356d
lxxxvi <lxxxvi@users.noreply.github.com>
2018-05-19 13:15:52 +0200

    split user#show into two partials
    - one for current_user
    - one for visiting another users page

--- test/integration/users_flows_test.rb
+++ test/integration/users_flows_test.rb
@@ -31,10 +31,25 @@ class UsersFlowsTest < ActionDispatch::IntegrationTest
 
     sign_in koebi
     get root_path
+    assert_response :success
 
     assert_select '.squad_members .squad_member' do
       assert_select '.ci-squad-name', 'CH Stars'
       assert_select '.ci-squad-ranking', 'You are ranked # 3 out of 3 players'
     end
+  end
+
+  test 'user sees only stats and squads on other users page' do
+    user = users(:zinedine)
+    other_user = users(:diego)
+
+    sign_in user
+    get user_path(other_user)
+    assert_response :success
+
+    assert_select 'h1', 'diego-maradona-11111'
+    assert_select 'h2', 'Stats'
+    assert_select 'h2', 'Squads'
+    assert_select 'h2', count: 2
   end
 end
