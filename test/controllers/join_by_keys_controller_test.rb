b530f26da0e249cb32d4ff1a8f78f6356c516808
Mario Schüttel <lxxxvi@users.noreply.github.com>
2018-06-02 15:24:48 +0200

    test that squad statistics changes after join by key (#113)

--- test/controllers/join_by_keys_controller_test.rb
+++ test/controllers/join_by_keys_controller_test.rb
@@ -3,35 +3,43 @@ require 'test_helper'
 class JoinByKeysControllerTest < ActionDispatch::IntegrationTest
   include SquadHelper
 
+  setup do
+    @user = users(:kubi)
+    @squad = squads(:ch_stars)
+  end
+
   test 'signed in kubi joins ch_stars through invitation link' do
-    user = users(:kubi)
-    squad = squads(:ch_stars)
+    sign_in @user
 
-    assert_not accepted_user_in_squad?(user, squad), 'Kubi should NOT be in squad before'
+    assert_not accepted_user_in_squad?(@user, @squad), 'Kubi should NOT be in squad before'
 
-    sign_in user
-
-    get squad_invitation_link(squad)
+    get squad_invitation_link(@squad)
     follow_redirect!
     assert :success
 
-    assert accepted_user_in_squad?(user, squad), 'Kubi SHOULD be in squad after'
+    assert accepted_user_in_squad?(@user, @squad), 'Kubi SHOULD be in squad after'
+  end
+
+  test 'squad rankings change after joined by key' do
+    sign_in @user
+
+    assert_changes '@squad.points_average', from: 0, to: 700 do
+      get squad_invitation_link(@squad)
+      @squad.reload
+    end
   end
 
   test 'cannot join through inviation link with wrong inviation key' do
-    user = users(:kubi)
-    squad = squads(:ch_stars)
+    assert_not accepted_user_in_squad?(@user, @squad), 'Kubi should NOT be in @squad before'
+    @squad.invitation_key = 'WRONGKEY'
 
-    assert_not accepted_user_in_squad?(user, squad), 'Kubi should NOT be in squad before'
-    squad.invitation_key = 'WRONGKEY'
+    sign_in @user
 
-    sign_in user
-
-    get squad_invitation_link(squad)
+    get squad_invitation_link(@squad)
     follow_redirect!
     assert :success
 
-    assert_not accepted_user_in_squad?(user, squad), 'Kubi SHOULD still NOT be in squad after'
+    assert_not accepted_user_in_squad?(@user, @squad), 'Kubi SHOULD still NOT be in squad after'
     assert_equal 'Not possible to join squad, did you use the right link?', flash[:notice]
   end
 
