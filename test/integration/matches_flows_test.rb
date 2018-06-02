30bdb5b14cf7b9cc22e67c1dccbb4ddea41094ca
lxxxvi <lxxxvi@users.noreply.github.com>
2018-06-02 18:15:58 +0200

    show kickoff time (kind of)

--- test/integration/matches_flows_test.rb
+++ test/integration/matches_flows_test.rb
@@ -23,4 +23,17 @@ class MatchesFlowsTest < ActionDispatch::IntegrationTest
       assert_not first_game.css('.score-controls').present?
     end
   end
+
+  test 'kickoff time is shown for upcoming match' do
+    sign_in users(:diego)
+    match = matches(:match_egy_uru)
+
+    travel_to (match.kickoff_at - 30.minutes) do
+      get prediction_center_path
+      assert_response :success
+      first_game = css_select('.match-with-prediction.future').first
+      first_game_kickoff_time = first_game.css('.ci-kickoff-time').first
+      assert_equal "Kickoff in\n\n30 minutes", first_game_kickoff_time.text.strip
+    end
+  end
 end
