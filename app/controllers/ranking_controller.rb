6e778c0cd63fadb913e869d89e2c77cb8626f212
lxxxvi <lxxxvi@users.noreply.github.com>
2018-06-17 18:21:44 +0200

    design cleanups and show squad ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,6 @@
 class RankingController < ApplicationController
   def index
-    @top_100 = User.ordered_by_ranking.limit(100)
+    @top_100_users  = User.ordered_by_ranking.limit(100)
+    @top_100_squads = Squad.ordered_by_ranking.limit(100)
   end
 end
