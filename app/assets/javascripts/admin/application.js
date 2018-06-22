b64c66cb933de187bd93268822ef009bcf7e50e9
mario <github@lxxxvi.ch>
2018-06-22 06:40:48 +0200

    hide finished cards, add toggle button

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -8,4 +8,22 @@ document.querySelectorAll("div[data-href]").forEach(function(element) {
   element.addEventListener("click", function() {
     window.location = this.dataset.href;
   });
+
+  const finishedMatchesCards = function() {
+    return document.querySelectorAll('.card.finished-match');
+  };
+
+  const toggleFinishedMatchesButton = function() {
+    return document.querySelector('.btn.toggle-finished-matches');
+  };
+
+  toggleFinishedMatchesButton().addEventListener('click', function() {
+    finishedMatchesCards().forEach(function(element) {
+      if(element.classList.contains('d-none')) {
+        element.classList.remove('d-none');
+      } else {
+        element.classList.add('d-none');
+      }
+    });
+  });
 });
