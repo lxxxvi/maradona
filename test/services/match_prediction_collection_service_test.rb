5a430701a2a45b0b50b2c5d457d5bef84862553e
Mario Schüttel <lxxxvi@users.noreply.github.com>
2018-06-24 18:49:01 +0200

    display next upcoming match in your locker (#137)

--- test/services/match_prediction_collection_service_test.rb
+++ test/services/match_prediction_collection_service_test.rb
@@ -2,12 +2,20 @@ require 'test_helper'
 
 class MatchPredictionCollectionServiceTest < ActiveSupport::TestCase
   test 'fetches all matches for user' do
+    service = MatchPredictionCollectionService.new(users(:diego))
     assert_equal 5, service.matches_with_predictions.count
   end
 
-  private
+  test 'fetches upcoming matches' do
+    match = matches(:match_bra_sui)
 
-  def service
-    MatchPredictionCollectionService.new(users(:diego))
+    travel_to match.kickoff_at - 1.minute do
+      service = MatchPredictionCollectionService.new(users(:diego), :next_upcoming)
+      assert_equal 1, service.matches_with_predictions.count
+      next_upcoming_match = service.matches_with_predictions.first
+
+      assert_equal 'BRA', next_upcoming_match.left_team.fifa_country_code
+      assert_equal 'SUI', next_upcoming_match.right_team.fifa_country_code
+    end
   end
 end
