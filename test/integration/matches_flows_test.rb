713befcf97993e9deff382c1ccbf5ee6cd22b988
Mario Schüttel <lxxxvi@users.noreply.github.com>
2018-07-17 12:04:58 +0200

    Hide upcoming matches, display passed matches after world cup (#144)

--- test/integration/matches_flows_test.rb
+++ test/integration/matches_flows_test.rb
@@ -4,11 +4,32 @@ class MatchesFlowsTest < ActionDispatch::IntegrationTest
   test 'landing page shows all games' do
     sign_in users(:diego)
 
-    get prediction_center_path
-    assert_response :success
-    assert_select '.match-with-prediction', { count: 5 }
-    assert_select '#prediction-center-actions .upcoming-matches.pushed', 'Upcoming'
-    assert_select '#prediction-center-actions .finished-matches', 'Finished'
+    travel_to during_the_world_cup do
+      get prediction_center_path
+      assert_response :success
+      assert_select '.match-with-prediction', { count: 5 }
+      assert_select '#prediction-center-actions .upcoming-matches.pushed', 'Upcoming'
+      assert_select '#prediction-center-actions .finished-matches', 'Finished'
+
+      hidden_passed_matches_count = css_select('.match-with-prediction.passed.d-none').count
+      assert hidden_passed_matches_count.positive?, 'Passed matches should be hidden if there are upcoming matches'
+    end
+  end
+
+  test 'prediction center doesnt show buttons to toggle between matches' do
+    sign_in users(:diego)
+
+    Match.where(left_team_score: nil).update_all(left_team_score: 0)
+    Match.where(right_team_score: nil).update_all(right_team_score: 0)
+
+    travel_to after_the_world_cup do
+      get prediction_center_path
+      assert_response :success
+
+      assert_select '.upcoming-matches', { count: 0 }
+      first_finished_match = css_select('.match-with-prediction.passed').first
+      assert_nil first_finished_match.classes.delete('d-none'), 'Finished matches should be displayed after world cup'
+    end
   end
 
   test 'match cannot be predicted anymore because kickoff is in past' do
